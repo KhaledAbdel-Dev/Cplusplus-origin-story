ce5af1af8070343335747e34f0a20665d7018f4f
khaled <khaledabdel.dev@gmail.com>
2023-01-14 19:22:43 -0500

    first lab!

---
+++ brownies.cpp
@@ -0,0 +1,28 @@
+/*
+Purpose: to count calories and macros per brownie serving
+Author: Khaled Abdelrahman
+Date: Feb 14, 2023
+*/
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    int eaterName = 'Khaled';
+    int mhmmmBrownie = 'Ghirardelli Dark Chocolate Brownie';
+
+    int calories = 140;
+    int fat = 3.5;
+    int carbs = 25;
+
+    int numOfServings = 3;
+
+    int totalCals = calories * numOfServings;
+    int totalFat = fat * numOfServings;
+    int totalCarbs = carbs * numOfServings;
+
+    cout << ""
+
+    return 0;
+}
